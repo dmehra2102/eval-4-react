c8b52c84df720b0252ac9a05b86e7f01cbc4b395
deepanshu mehra <deepanshumehra2102@gmail.com>
2022-06-13 16:22:49 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ButtonComponent from "./components/ButtonComponent";
+import CityRow from "./components/CityRow";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([{}]);
+  const [page,setpage] = useState(1);
+  const [sorting,setsorting] = useState(true);
+
+// <div id="loading-container"></div>
+
+  useEffect(() => {
+        fetch(`https://json-server-mocker-masai.herokuapp.com/cities?_limit=10&_page=${page}`)
+      .then((res)=> res.json())
+      .then(datas=> {
+        if(sorting){
+          return datas.sort((a,b)=> a.population-b.population);
+        }
+        return datas.sort((a,b)=> b.population-a.population);
+      })
+      .then((datas)=> setData(datas));
+      console.log(data);
+
+  }, [page,sorting]);
+
+  
+
+  const handlechange = (inc)=>{
+    setpage(page+inc); 
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div id="loading-container"></div> */}
+      <table>
+        <tr>
+          <th>
+            ID
+          </th>
+          <th>
+            CITY NAME
+          </th>
+          <th>
+            COUNTRY NAME
+          </th>
+          <th>
+            POPULATION
+          </th>
+          </tr>
+          {
+            data.map((item)=> {
+                return <CityRow item={item}/>              
+            })
+          }
+      </table>
+
+      <div>{
+        page!==1 && (
+           <ButtonComponent title="PREV" id="PREV" onClick={()=> handlechange(-1)}/>
+          )
+        }
+        <ButtonComponent id="SORT_BUTTON" title={!sorting ? "Sort by Increasing Population": "Sort by Descending Population"} onClick={()=> setsorting(!sorting)}/>
+        {
+          page!==5 && (
+            <ButtonComponent id="NEXT" title="NEXT" page={page} setpage={setpage} onClick={()=> handlechange(1)}/>
+          )
+        }
+      </div>
     </div>
   );
 }
-
-export default App;
